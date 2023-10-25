8d1948f69fb2ead24bae3e5b67cfbd70b8d75831
me_madness <p_rangel@abv.bg>
2023-10-25 20:06:59 +0200

    Beginig of task number 03

--- 03. email.py
+++ 03. email.py
@@ -1,5 +1,27 @@
 # First task from the lecture
 
+class Email:
+    def __init__(self, sender, receiver, content):
+       self.sender = sender
+       self.receiver = receiver
+       self.content = content
+       self.is_sent = False
+ 
+ 
+## Input one
+# Peter John Hi,John
+# John Peter Hi,Peter!
+# Katy Lilly Hello,Lilly
+# Stop
+# 0, 2
+
+## Input two
+# Anna, Bella, Hi
+# Sam, Dany, Okey
+# Felix, Mery, Bye
+# Stop
+# 0
+
 
 
 # Second task from me

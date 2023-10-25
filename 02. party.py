08284187d72bad9f1ff390d8c62fd5c341272ccc
me_madness <p_rangel@abv.bg>
2023-10-25 19:50:38 +0200

    Go go Go

--- 02. party.py
+++ 02. party.py
@@ -4,8 +4,11 @@ class Party:
     def __init__(self):
         self.people = []
         
-        
-command = input().split(", ")
+party = Party()        
+name = input()
+while name != "End":
+    party.people.append(name)
+    name = input()
 
 
         

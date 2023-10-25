502fe3cd5d37300954546987eed8177613a1b17e
me_madness <p_rangel@abv.bg>
2023-10-25 19:34:14 +0200

    start with first

--- 01. comment.py
+++ 01. comment.py
@@ -1,5 +1,10 @@
 # First task from the lecture
 
-
+class Comment:
+  def __init__(self, username, content, likes = 0):
+      self.username = username
+      self.content = content
+      self.likes = likes
+        
 
 # Second task from me

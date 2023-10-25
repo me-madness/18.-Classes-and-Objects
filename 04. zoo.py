4055625b2d1e479293dad42bc92d8eeffdc5532a
me_madness <p_rangel@abv.bg>
2023-10-25 20:53:59 +0200

    04 zoo

--- 04. zoo.py
+++ 04. zoo.py
@@ -1,10 +1,32 @@
 # First task from the lecture
 
 class Zoo:
-    def __init__(self):
-     pass
-
-
+    __animals = 0
+    def __init__(self,name):
+        self.name = name
+        self.mammals = []
+        self.fishes = []
+        self.birds = []
+        
+    
+    def add_animal(self, species, name):
+        if species == "mammal":
+            self.mammals.append(name)
+        elif species == "fish":
+            self.fishes.append(name)
+        elif species == "bird":
+            self.birds.append(name)    
+    
+    def get_info(self, species):
+        pass
+    
+    
+zoo_name = input() 
+zoo_object = Zoo(zoo_name)       
+count_of_animals = int(input())
+for animals in range(count_of_animals):
+    species, name = input().split()
+    zoo_object.add_animal(species, name)
 
 
 ## Input one

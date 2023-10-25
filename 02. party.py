60bdd94a38b3cd1d930bd16c38b401b402a5a5c4
me_madness <p_rangel@abv.bg>
2023-10-25 19:48:27 +0200

    02. on the way

--- 02. party.py
+++ 02. party.py
@@ -1,5 +1,25 @@
 # First task from the lecture
 
+class Party:
+    def __init__(self):
+        self.people = []
+        
+        
+command = input().split(", ")
 
 
-# Second taks from me
+        
+##Input one
+# Peter
+# John
+# Katy
+# End
+
+## Input two
+# Sam
+# Eddy
+# Edd
+# Kris
+# End
+
+# Second task from me

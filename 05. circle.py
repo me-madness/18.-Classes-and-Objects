3d259b41a0d5a954ff15eedff55ecac00e78fa1a
me_madness <p_rangel@abv.bg>
2023-10-25 20:33:13 +0200

    05. more two task to finish the lecture

--- 05. circle.py
+++ 05. circle.py
@@ -1,5 +1,18 @@
 # First task from the lecture
 
+class Circle:
+    def __init__(self):
+        pass
+    
+    
+    
+## Input 
+# circle = Circle(10)
+# angle = 5
 
+# print(f"{circle.calculate_circumference():.2f}")
+# print(f"{circle.calculate_area():.2f}")
+# print(f"{circle.calculate_area_of_sector(angle):.2f}")
+   
 
 # Second task from me

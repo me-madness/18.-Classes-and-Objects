85caf3cd9463509d44ba1c94ec2b822c12af1b5e
me_madness <p_rangel@abv.bg>
2023-10-25 19:35:45 +0200

    01. done

--- 01. comment.py
+++ 01. comment.py
@@ -1,10 +1,15 @@
 # First task from the lecture
-
 class Comment:
   def __init__(self, username, content, likes = 0):
       self.username = username
       self.content = content
       self.likes = likes
-        
+      
+      
+comment = Comment("user1", "I like this book")
+print(comment.username)
+print(comment.content)
+print(comment.likes)
+       
 
 # Second task from me

086cb68322ec5320598e2410c52168549dfaaff2
me_madness <p_rangel@abv.bg>
2023-10-25 21:02:20 +0200

    04. Done by the lecture

--- 04. zoo.py
+++ 04. zoo.py
@@ -16,9 +16,20 @@ class Zoo:
             self.fishes.append(name)
         elif species == "bird":
             self.birds.append(name)    
-    
+
+        Zoo.__animals += 1
+        
+        
     def get_info(self, species):
-        pass
+        string_for_print = ""
+        if species == "mammal":
+            string_for_print += f"Mammals in {self.name}: {', '.join(self.mammals)}"
+        elif species == "fish":
+            string_for_print += f"Fishes in {self.name}: {', '.join(self.fishes)}"
+        elif species == "bird":
+            string_for_print += f"Birds in {self.name}: {', '.join(self.birds)}"
+        string_for_print += f"\nTotal animal: {Zoo.__animals}"
+        return string_for_print
     
     
 zoo_name = input() 
@@ -27,7 +38,8 @@ count_of_animals = int(input())
 for animals in range(count_of_animals):
     species, name = input().split()
     zoo_object.add_animal(species, name)
-
+searched_species = input()
+print(zoo_object.get_info(searched_species))
 
 ## Input one
 # Great Zoo

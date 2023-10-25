093e7e64aeaf20431a115026226a829289988b0d
me_madness <p_rangel@abv.bg>
2023-10-25 20:25:02 +0200

    04. Let's start with next task

--- 04. zoo.py
+++ 04. zoo.py
@@ -1,5 +1,27 @@
-# First task from the lucture
+# First task from the lecture
 
+class Zoo:
+    def __init__(self):
+     pass
+
+
+
+
+## Input one
+# Great Zoo
+# 5
+# mammal lion
+# mammal bear
+# fish salmon
+# bird owl
+# mammal tiger
+# mammal
+
+## Input two
+# Blah
+# 1
+# mammal bear
+# mammal
 
 
 # Second task from me
